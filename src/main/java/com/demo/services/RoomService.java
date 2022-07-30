7e205887dcc77ae58a24092547a4f08d00829f7a
PhanHoangLocAG <phanhoangloc2017@gmail.com>
2022-07-30 17:15:39 +0700

    loc commit 30/07

--- src/main/java/com/demo/services/RoomService.java
+++ src/main/java/com/demo/services/RoomService.java
@@ -136,5 +136,10 @@ public class RoomService implements IRoomService {
         return roomRepository.findRoomByAccommodationId(accommodationId);
     }
 
+    @Override
+    public List<RoomApi> findRoomByCityDateRequest(String nameCity, Date from, Date to, int capacity, int childrenQuantity, int adultQuantity) {
+        return roomRepository.findRoomByCityDateRequest(nameCity, from, to, capacity, childrenQuantity, adultQuantity);
+    }
+
 
 }
